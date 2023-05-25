125f71bf7b5e479817e656986f043f2e9124e049
Rakdoslover <>
2023-05-25 08:17:32 +0000

    something is wrong and I don't know what I've done!

--- scripts/game.js
+++ scripts/game.js
@@ -3,20 +3,26 @@ let game = {
     playerMoves: [],
     score: 0,
     turnNumber: 0,
+    lastButton: "",
+    turnInProgress: false,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
 function newGame() {
-    game.score = 0;
-    game.playerMoves = [];
     game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+
     for (let circle of document.getElementsByClassName("circle")) {
-        if(circle.getAttribute("data-listener") !== "true") {
+        if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
-                let move = e.target.getAttribute("id");
-                lightsOn(move);
-                game.playerMoves.push(move);
-                playerTurn();
+                if (game.currentGame.length > 0 && !game.turnInProgress); {
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    lightsOn(move);
+                    game.playerMoves.push(move);
+                    playerTurn();
+                }
             });
             circle.setAttribute("data-listener", "true");
         }
@@ -25,32 +31,48 @@ function newGame() {
     addTurn();
 }
 
-function showScore() {
-    document.getElementById("score").innerText = game.score;
-}
-
 function addTurn() {
-    game.playerMoves = []
+    game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
     showTurns();
 }
 
+function showTurns() {
+    game.turnInProgress = true;
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+            game.turnInProgress = false;
+        }
+    }, 800);
+}
+
 function lightsOn(circ) {
     document.getElementById(circ).classList.add("light");
-    setTimeout(() => {
+    setTimeout(function () {
         document.getElementById(circ).classList.remove("light");
     }, 400);
 }
 
-
-function showTurns() {
-    game.turnNumber =   0;
-    let turns = setInterval(() => {
-        lightsOn(game.currentGame[game.turnNumber]);
-        game.turnNumber++;
-        if(game.turnNumber >= game.currentGame.length) {
-            clearInterval(turns);
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
         }
-    }, 800);
+    } else {
+        alert("Wrong move!");
+        newGame();
+    }
 }
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
